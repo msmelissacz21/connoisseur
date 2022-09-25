523f120398b65c42c6a93ad6dad3b7fe418dadee
Melissa Zuengler <melissazuengler@Melissas-MacBook-Pro.local>
2022-09-24 19:46:42 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Connisseur - all things food
+
+This is a React, TS, Scss, node application.
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Scss/style.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Connoisseur
+      </h1>
+    </div>
+  );
+}
+
+export default App;
